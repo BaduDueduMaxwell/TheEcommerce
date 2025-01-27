67464cf677ac68fd7711766634d1727b71abdcf0
kode <duedumaxwell43@gmail.com>
2025-01-26 12:03:39 -1200

    Refactor: optimized index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Load environment variables
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -6,25 +9,21 @@ const userRoute = require("./routes/user.route.js");
 const cartRoute = require("./routes/cart.route.js");
 const orderRoute = require("./routes/order.route.js");
 const paymentRoute = require("./routes/payment.route.js");
+
 const app = express();
 
 // middleware
 app.use(express.json());
-
-// Load environment variables
-require("dotenv").config();
 
 // routes
 app.use("/api/products", productRoute);
 app.use("/api/users", userRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("api/payment", paymentRoute);
+app.use("/api/payment", paymentRoute);
 
 mongoose
-  .connect(
-    "mongodb://duedumaxwell63:meLWpeHd83MilMYK@backenddb-shard-00-00.pasoy.mongodb.net:27017,backenddb-shard-00-01.pasoy.mongodb.net:27017,backenddb-shard-00-02.pasoy.mongodb.net:27017/?ssl=true&replicaSet=atlas-5hbovb-shard-0&authSource=admin&retryWrites=true&w=majority&appName=BackendDB"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to the database");
     app.listen(3000, () => {
